8b40b2779c4bfa480daa34fd5df34ef4ae320394
Ricardo <damaceno.ric@gmail.com>
2022-09-07 19:03:14 -0300

    início do projeto

---
+++ aposta-tozo/src/main/java/com/apostas/Gols.java
@@ -0,0 +1,29 @@
+package com.apostas;
+
+public class Gols implements Comparable<Gols>{
+
+	private int gol;
+	
+	private String quantidade = "";
+
+	public Gols(int gol) {
+		this.gol = gol;
+	}
+
+
+	public int getGol() {
+		return gol;
+	}
+	
+	@Override
+	public String toString() {
+		return this.quantidade + gol;
+	}
+
+
+	@Override
+	public int compareTo(Gols gols) {
+		return getGol();
+	}
+	
+}
